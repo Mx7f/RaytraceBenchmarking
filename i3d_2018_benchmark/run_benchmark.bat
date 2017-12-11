74726d03ea2245d691e44cf0f6b67171f8e5456b
Michael Mara <Michael.Mara@Oculus.com>
2017-12-10 19:36:56 -0800

    Improving project file, and new benchmark script.

---
+++ i3d_2018_benchmark/run_benchmark.bat
@@ -0,0 +1,7 @@
+SET benchmarkTool=..\x64\Release\RaytraceBenchmarking.exe
+
+%benchmarkTool% home.offb home.rffb
+%benchmarkTool% bunny.offb bunny.rffb
+%benchmarkTool% conference.offb conference.rffb
+%benchmarkTool% sponza.offb sponza.rffb
+%benchmarkTool% bistro.offb bistro.rffb
